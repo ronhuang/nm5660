9656f5b1b21fc3a4c400ccbad6daa86180b5b3fc
Ron Huang <ronhuang@gmail.com>
2010-04-19 16:43:35 +0800

    Initial import.

---
+++ User Experience and Transparency.mm
@@ -0,0 +1,9 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1271223837484" ID="ID_1595341551" MODIFIED="1271223863500" TEXT="User Experience and Transparency">
+<node CREATED="1271223887654" ID="ID_526394483" MODIFIED="1271223904041" POSITION="right" TEXT="Emotional Design">
+<node CREATED="1271223905058" ID="ID_1321225121" MODIFIED="1271224044529" TEXT="The problem here is that you don&apos;t know what to expect. The manufacturers promist all sorts of wonderful results; but, in fact, the technology and its operations are invisible, mysteriously hidden from view, and often completely arbitrary, secretive, and sometimes even contradictory. (page 140)"/>
+<node CREATED="1271224665235" ID="ID_1395898887" MODIFIED="1271224675536" TEXT="Trust (page 141)"/>
+</node>
+</node>
+</map>
